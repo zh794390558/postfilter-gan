798f015a350fb0d23584118f60adae0dc28716e8
Hui Zhang <zhanghui@unisound.com>
2017-07-24 17:34:18 +0800

    fix docker build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,26 @@
 
 set -x 
 
-docker build --force-rm=false --no-cache=false -f docker/Dockerfile -t harbor.ail.unisound.com/zhanghui/tensorflow-cpu-1.2.1 docker 
+if [[ $# < 1 ]]; then
+	echo 'usage: make [gpu|cpu]'
+	exit 1
+fi
+
+if [[ $1 == cpu ]]; then
+	FILE=docker/Dockerfile
+elif [[ $1 == gpu ]]; then
+	FILE=docker/Dockerfile.gpu
+else
+	echo "invalid pararm: $1"
+	exit 1
+fi
+
+
+docker build --force-rm=false --no-cache=false -f $FILE -t harbor.ail.unisound.com/zhanghui/tensorflow-$1-1.2.1 docker 
 
 if [ $? != 0 ]; then
 	exit 1
 fi
 
-docker rmi harbor.ail.unisound.com/zhanghui/tensorflow-cpu-1.2.1:latest
-docker push harbor.ail.unisound.com/zhanghui/tensorflow-cpu-1.2.1 
+#docker rmi harbor.ail.unisound.com/zhanghui/tensorflow-$1-1.2.1:latest
+docker push harbor.ail.unisound.com/zhanghui/tensorflow-$1-1.2.1 
